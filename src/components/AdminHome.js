d0363089e5e5409b1dc8c8455327bfedb73711bd
Drake <dgoldsmith@csumb.edu>
2023-09-28 19:11:59 -0700

    Solution to Assignment 3

--- src/components/AdminHome.js
+++ src/components/AdminHome.js
@@ -1,25 +1,151 @@
 import React, { useState, useEffect } from 'react';
+import AddStudent from './AddStudent';
+import EditStudent from './EditStudent';
+import { SERVER_URL } from '../constants';
 
-const AdminHome = ()  => {
+const AdminHome = () => {
+    const [students, setStudents] = useState([]); // State variable to store the list of students
+    const [message, setMessage] = useState(''); // State variable for status messages
 
     useEffect(() => {
-        // called once after intial render
-        fetchStudents();
-        }, [] )
+        fetchStudents(); // Fetch the list of students when the component mounts
+    }, []);
 
-
+    // Function to fetch the list of students from the server
     const fetchStudents = () => {
-		//TODO complete this method to fetch students and display list of students
-    }
+        fetch(`${SERVER_URL}/student`) // Make a GET request to the server using the SERVER_URL
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Failed to fetch students (${response.status})`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                // Update the 'students' state with the fetched data
+                setStudents(data);
+            })
+            .catch((error) => {
+                console.error(`Error fetching students: ${error}`);
+                setMessage('Error fetching students. Please try again.');
+            });
+    };
 
+    // Function to add a new student
+    const addStudent = (student) => {
+        setMessage('');
+        console.log('start addStudent');
+        fetch(`${SERVER_URL}/student`, {
+            method: 'POST', // HTTP POST request to add a new student
+            headers: {
+                'Content-Type': 'application/json', // Specify JSON data in the request body
+            },
+            body: JSON.stringify(student), // Send the student data in the request body as JSON
+        })
+            .then((res) => {
+                if (res.ok) {
+                    console.log('addStudent ok');
+                    setMessage('Student added.');
+                    fetchStudents(); // Refresh the student list after adding
+                } else {
+                    console.log('error addStudent ' + res.status);
+                    setMessage('Error. ' + res.status);
+                }
+            })
+            .catch((err) => {
+                console.error('exception addStudent ' + err);
+                setMessage('Exception. ' + err);
+            });
+    };
+
+    // Function to edit an existing student
+    const editStudent = (updatedStudent) => {
+        setMessage('');
+        const student_id = students[0].studentId; // Assuming you're editing the first student
+        console.log('start editStudent');
+        fetch(`${SERVER_URL}/student/${student_id}`, {
+            method: 'PUT', // HTTP PUT request to update the student
+            headers: {
+                'Content-Type': 'application/json', // Specify JSON data in the request body
+            },
+            body: JSON.stringify(updatedStudent), // Send the updated student data as JSON
+        })
+            .then((res) => {
+                if (res.ok) {
+                    console.log('editStudent ok');
+                    setMessage('Student edited.');
+                    fetchStudents(); // Refresh the student list after editing
+                } else {
+                    console.log('error editStudent ' + res.status);
+                    setMessage('Error. ' + res.status);
+                }
+            })
+            .catch((err) => {
+                console.error('exception editStudent ' + err);
+                setMessage('Exception. ' + err);
+            });
+    };
+
+    // Function to delete a student
+    const deleteStudent = (event) => {
+        setMessage('');
+        const row_id = event.target.parentNode.parentNode.rowIndex - 1;
+        console.log("drop course " + row_id);
+        const student_id = students[row_id].studentId;
+        if (window.confirm('Are you sure you want to delete the student?')) {
+            //console.log("Constructed URL: ", `${SERVER_URL}/student/${student_id}?force=true`);
+            fetch(`${SERVER_URL}/student/${student_id}?force=true`, {
+                method: 'DELETE', // HTTP DELETE request to delete the student
+            })
+                .then((res) => {
+                    if (res.ok) {
+                        console.log('delete ok');
+                        setMessage('Student deleted.');
+                        fetchStudents(); // Refresh the student list after deletion
+                    } else {
+                        console.log('delete error');
+                        setMessage('Error deleteStudent. ' + res.status);
+                    }
+                })
+                .catch((err) => {
+                    console.log('exception deleteStudent ' + err);
+                    setMessage('Exception. ' + err);
+                });
+        }
+    };
+
+    const headers = ['Student_Id', 'Name', 'email', 'Status', 'Status Code', ' ', ' '];
 
     return (
-        <div> 
-        <div margin="auto" >
-          <h3>Student List</h3>
+        <div margin="auto">
+            <h3>Students List</h3>
+            <h4>{message}</h4>
+            <table className="Center">
+                <thead>
+                    <tr>
+                        {headers.map((s, idx) => (<th key={idx}>{s}</th>))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {students.map((row, idx) => (
+                        <tr key={idx}>
+                            <td>{row.studentId}</td>
+                            <td>{row.name}</td>
+                            <td>{row.email}</td>
+                            <td>{row.status}</td>
+                            <td>{row.statusCode}</td>
+                            <td>
+                                <EditStudent initialStudent={row} editStudent={editStudent} />
+                            </td>
+                            <td>
+                                <button type="button" margin="auto" onClick={deleteStudent}>Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <AddStudent addStudent={addStudent} />
         </div>
-      </div>
-    )
+    );
 }
 
 export default AdminHome;
--- src/components/EditStudent.js
+++ src/components/EditStudent.js
@@ -1,12 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
-const EditStudent = (props)  => {
+const EditStudent = (props) => {
+    const [open, setOpen] = useState(false);
+    const [student, setStudent] = useState({
+        // Initialize a student object with editable fields
+        name: '',
+        email: '',
+        status: '',
+        statusCode: '',
+    });
 
+    useEffect(() => {
+        // Update the student state with the initialStudent prop data
+        setStudent({ ...props.initialStudent });
+    }, [props.initialStudent]);
+
+    // Function to open the dialog
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    // Function to close the dialog
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    // Function to handle changes in input fields
+    const handleChange = (event) => {
+        // Get the name and value of the changed input field
+        const { name, value } = event.target;
+        setStudent({
+            ...student,
+            [name]: value, // Update the specific field using its name
+        });
+    };
+
+    // Function to handle the edit action
+    const handleEdit = () => {
+        props.editStudent(student);
+        // Close the dialog
+        handleClose();
+    };
 
     return (
         <div>
-        </div>                       
-    )
+            <Button variant="outlined" color="primary" style={{ margin: 10 }} onClick={handleClickOpen}>
+                Edit Student
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Edit Student</DialogTitle>
+                <DialogContent style={{ paddingTop: 20 }}>
+                    <TextField fullWidth label="Name" name="name" value={student.name} onChange={handleChange} />
+                    <TextField fullWidth label="Email" name="email" value={student.email} onChange={handleChange} />
+                    <TextField fullWidth label="Status" name="status" value={student.status || ''} onChange={handleChange} />
+                    <TextField fullWidth label="Status Code" name="statusCode" value={student.statusCode} onChange={handleChange} />
+                </DialogContent>
+                <DialogActions>
+                    <Button color="secondary" onClick={handleClose}>
+                        Cancel
+                    </Button>
+                    <Button color="primary" onClick={handleEdit}>
+                        Edit
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 }
+
+EditStudent.propTypes = {
+    editStudent: PropTypes.func.isRequired,
+};
 
 export default EditStudent;
